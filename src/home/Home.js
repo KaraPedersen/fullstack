c8ed517c043a7447b8bbbb87e2a83f96da98ecc4
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-05-10 15:01:30 -0700

    added home page and styling

--- src/home/Home.js
+++ src/home/Home.js
@@ -7,9 +7,12 @@ export default class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h2>Home Page</h2>
-
-        <Link to='/resources'>See the List</Link>
+        <header>
+          <h2>Home Page</h2>
+        </header>
+        <p>
+          <Link to='/dogs'>See the List</Link>
+        </p>
       </div>
     );
   }

caf76adae0f44abbf2e158b1368a60b89c7008ac
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-05-10 16:40:16 -0700

    added list page and styling

---
+++ src/dogs/DogItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './DogItem.css';
+
+class DogItem extends Component {
+
+  render() {
+    const dog = this.props.dog;
+
+    return (
+      <li className="DogItem">
+        <Link to={`/dogs/${dog.id}`}>
+          <h2>{dog.name}</h2>
+          <img src={dog.url} alt={dog.name}/>
+          <p> TvShows: {dog.tvShow}</p>
+        </Link>
+      </li>
+    );
+  }
+  
+}
---
+++ src/dogs/DogList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import DogItem from './DogItem';
+import './DogList.css';
+
+class DogList extends Component {
+  
+  render() {
+    const { dogs } = this.props;
+
+    return (
+      <ul className="DogList">
+        {dogs.map(dog => (
+          <DogItem key={dog.id} dog={dog}/>
+
+        ))}
+      </ul>
+    );
+  }
+  
+}
